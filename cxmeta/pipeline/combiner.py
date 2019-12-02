91b4803f963f150275ba68234299ab5131bb4bc5
Jacob Repp <jacobrepp@gmail.com>
2019-12-02 00:16:40 -0800

    Progressing combiner

--- cxmeta/pipeline/combiner.py
+++ cxmeta/pipeline/combiner.py
@@ -1,55 +1,78 @@
 import itertools
 
 from .line_processor import LineProcessor
-from .cxx_comments import CommentProcessor
-from .cxx_statements import StatementProcessor
+from .cxx_processor import Processor
+from .stream import Stream
 
 
 class Chunk(object):
-    def __init__(self, comment, stmt):
-        self.comment = comment
-        self.stmt = stmt
+    def __init__(self):
+        self.comment = None
+        self.stmt = None
+        self.class_name = None
+        self.line_num = 0
 
     def __str__(self):
-        return "{}\n{}".format(self.coment, self.stmt)
+        return "COMMENTS: {}\nSTATEMENTS: {}\n".format(self.comment, self.stmt)
 
 
 class Combiner(LineProcessor):
     def __init__(self, source):
-        self.comment_proc = CommentProcessor(source)
-        self.stmt_proc = StatementProcessor(source)
-        self.comments = None
-        self.stmts = None
-        self.linenum = 0
+        self.proc = Processor(source)
+        self.stream_data = Stream(source)
+        self.chunk = None
+        self.comments = list()
+        self.statements = list()
 
     def process_line(self, line):
-        self.comment_proc.process_line(line)
-        self.stmt_proc.process_line(line)
+        self.proc.process_line(line)
+
+    def finish_chunk(self):
+        if self.chunk.class_name:
+            self.chunk.comment = ''.join(self.comments)
+            self.chunk.stmt = ''.join(self.statements)
+            self.stream_data.append(self.chunk.line_num, 0, self.chunk)
+            self.chunk = Chunk()
+            self.comments = list()
+            self.statements = list()
+
+    def comment_handler(self, atom):
+        content = atom.data[r'content']
+        self.comments.append(content)
+        if content.strip().startswith('..class::'):
+            self.chunk.class_name = content.split('::')[-1].strip()
+            self.chunk.line_num = atom.line_num
+
+    def block_end_handler(self, _):
+        self.finish_chunk()
+
+    def statement_end_handler(self, _):
+        self.finish_chunk()
+
+    def content_handler(self, atom):
+        self.statements.append(atom.data[r'content'])
+
+    def default_handler(self, atom):
+        if type(atom.data) is dict:
+            content = atom.data.get(r'content')
+            if content:
+                self.statements.append(content)
 
     def combine(self):
-        comments = self.comment_proc.stream().read()
-        stmts = self.stmt_proc.stream().read()
+        self.chunk = Chunk()
+        self.comments = list()
+        self.statements = list()
 
-        # discover chunks to extract from the comment stream
-        for comment in comments:
-            if not comment.data['content'].strip().startswith('..class::'):
-                next
-
-            # find the first related statement
-            for stmt in itertools.takewhile(lambda x: x.linenum <= comment.linenum, stmts):
-                pass
-
-            # combine all comments up to first statement
-            related_comments = list()
-            related_comments.extend(itertools.takewhile(lambda x: x.linenum <= stmt.linenum, comments))
-
-            # combine all statements till end of block
-            related_stmts = list()
-            related_stmts.extend(itertools.takewhile(lambda x: x.data['type'] != r'block-end', stmts))
-
-            chunk = Chunk('\n'.join(comments), '\n'.join(stmts))
-            self.stream.append(comment.linenum, 0, chunk)
+        handlers = {
+            r'content': self.content_handler,
+            r'comment': self.comment_handler,
+            r'block-end': self.block_end_handler,
+            r'stmt-end': self.statement_end_handler,
+        }
+        for atom in self.proc.stream().read():
+            handler = handlers.get(atom.data[r'type']) or self.default_handler
+            handler(atom)
 
     def stream(self):
-        return self.stream
+        return self.stream_data
 
