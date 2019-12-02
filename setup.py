6775f191e084cc408402b7c88f7165c029615b04
Jacob Repp <jacob.repp@cloudkitchens.com>
2019-12-02 00:45:05 -0800

    Updating setup/requirements

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+setup(
+    name="cxmeta",
+    version="0.1",
+    packages=find_packages(),
+    scripts=['cxmeta/tools/cli.py'],
+    install_requires=[
+        'docopt',
+        'docutils',
+    ],
+    package_data={
+        '': ['*.md', '*.rst']
+    },
+    author='Jacob Repp',
+    author_email='jacobrepp@gmail.com',
+    description='A python CLI and library to extract meta-data from Cxx style languages.',
+    keywords='Cxx C C++ documentation docs rst markdown build',
+    url='https://github.com/jrepp/cxmeta',
+    project_urls={
+        'Source Code': 'https://github.com/jrepp/cxmeta'
+    },
+    classifiers=[
+        'License :: LGPL 3.0 ::',
+    ]
+)
