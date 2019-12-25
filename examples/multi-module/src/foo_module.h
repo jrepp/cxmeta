7425667bbc4abed613c82f16c991c3c384ada537
Jacob Repp <jacobrepp@gmail.com>
2019-12-25 22:49:13 +0100

    Removing rst directives

--- examples/multi-module/src/foo_module.h
+++ examples/multi-module/src/foo_module.h
@@ -2,13 +2,14 @@
 #ifndef _foo_module_h
 #define _foo_module_h
 
-// ..class:: cxm_function
-// foo function 1 has some text
+// Foo function 1 is not very important but I'd like for you to know what it does.
+// So I will explain it here:
+//
 // * It's very important to the system
 // * Sometimes people forget about it
 void foo_function_1(void);
 
-/**** ..class:: cxm_function
+/****
  * foo function 2 has some more text
  *
  * * We had to add this because foo function 1 was too
